16c7e1d278b91e3a41334280834d1f6e1e3069a4
Mohamed <mokachtit@gmail.com>
2024-06-30 18:32:38 +0200

    feat: deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,18 @@
-// middleware.ts
+// src/middleware.ts
+
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import createMiddleware from 'next-intl/middleware';
 
 import { AllLocales, AppConfig } from './utils/AppConfig';
 
+// Initialize the internationalization middleware
 const intlMiddleware = createMiddleware({
-  locales: AllLocales,
-  localePrefix: AppConfig.localePrefix,
-  defaultLocale: AppConfig.defaultLocale,
+  locales: AllLocales, // List of supported locales
+  localePrefix: AppConfig.localePrefix, // Configuration for locale prefix
+  defaultLocale: AppConfig.defaultLocale, // Default locale
 });
 
+// Define route patterns for protected routes
 const isProtectedRoute = createRouteMatcher([
   '/dashboard(.*)',
   '/:locale/dashboard(.*)',
@@ -17,11 +20,15 @@ const isProtectedRoute = createRouteMatcher([
   '/:locale/settings(.*)',
 ]);
 
+// Main middleware function that applies Clerk authentication and internationalization
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-  return intlMiddleware(req);
+  if (isProtectedRoute(req)) {
+    auth().protect(); // Protect the route with Clerk authentication
+  }
+  return intlMiddleware(req); // Apply internationalization middleware
 });
 
+// Configuration for the middleware to match specific paths
 export const config = {
   matcher: [
     // Exclude files with a "." followed by an extension, which are typically static files.
@@ -30,4 +37,5 @@ export const config = {
     // Re-include any files in the api or trpc folders that might have an extension
     '/(api|trpc)(.*)',
   ],
+  runtime: 'edge', // Ensure the middleware uses the Edge runtime
 };
